cb05f6ca7576d4070fcbe7fe3cac0f6a944b9421
tawnaylor <nay19001@byui.edu>
2025-10-04 17:29:46 -0600

    start new 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,86 @@
-import { getParkData } from "./parkService.mjs";
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
 
 const parkData = getParkData();
 
-// Update disclaimer link
-const disclaimer = document.querySelector(".disclaimer a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
+}
 
-// Update page title
-document.title = parkData.fullName;
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
 
-// Update hero image
-const heroImage = document.querySelector(".hero-banner__image");
-heroImage.src = parkData.images[0].url;
-heroImage.alt = parkData.images[0].altText;
-
-// Update park info (name, designation, states)
-const heroInfo = document.querySelector(".hero-banner__info");
-heroInfo.innerHTML = parkInfoTemplate(parkData);
-
-function parkInfoTemplate(info) {
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
+export function parkInfoTemplate(data) {
   return `
-    <a href="${info.url}" class="hero-banner__title">${info.name}</a>
-    <p class="hero-banner__subtitle">
-      <span>${info.designation}</span> |
-      <span>${info.states}</span>
-    </p>
+    <h2>${data.fullName}</h2>
+    <p>${data.description}</p>
   `;
+}
+
+export function mediaCardTemplate(info) {
+  return `
+    <div class="media-card">
+      <a href="${info.link}">
+        <img src="${info.image}" alt="${info.name}" class="media-card__img">
+        <h3 class="media-card__title">${info.name}</h3>
+      </a>
+      <p>${info.description}</p>
+    </div>`;
+}
+
+export function getMailingAddress(addresses) {
+  return addresses.find((address) => address.type === "Mailing");
+}
+
+export function getVoicePhone(numbers) {
+  const voice = numbers.find((number) => number.type === "Voice");
+  return voice.phoneNumber;
+}
+
+export function footerTemplate(info) {
+  const mailing = getMailingAddress(info.addresses);
+  const voice = getVoicePhone(info.contacts.phoneNumbers);
+
+  return `
+    <section class="contact">
+      <h3>Contact Info</h3>
+      <h4>Mailing Address:</h4>
+      <div>
+        <p>${mailing.line1}</p>
+        <p>${mailing.city}, ${mailing.stateCode} ${mailing.postalCode}</p>
+      </div>
+      <h4>Phone:</h4>
+      <p>${voice}</p>
+    </section>`;
+}
+import { parkInfoTemplate, footerTemplate } from "./templates.mjs";
+
+function setHeaderInfo(data) {
+  const disclaimer = document.querySelector(".disclaimer > a");
+  disclaimer.href = data.url;
+  disclaimer.innerHTML = data.fullName;
+
+  document.querySelector("head > title").textContent = data.fullName;
+  document.querySelector(".hero-banner > img").src = data.images[0].url;
+
+  document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(data);
+}
+
+function setFooter(data) {
+  const footerEl = document.querySelector("#park-footer");
+  footerEl.innerHTML = footerTemplate(data);
+}
+
+export default function setHeaderFooter(data) {
+  setHeaderInfo(data);
+  setFooter(data);
 }
--- src/js/parkService.mjs
+++ src/js/parkService.mjs
@@ -1,70 +1,26 @@
-const park = {
-  fullName: "Yellowstone National Park",
-  name: "Yellowstone",
-  designation: "National Park",
-  description:
-    "Yellowstone National Park is a nearly 3,500-sq.-mile wilderness recreation area atop a volcanic hot spot.",
-  states: "Wyoming, Montana, Idaho",
-  url: "https://www.nps.gov/yell/index.htm",
-  images: [
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5920-1DD8-B71B-0B83F012ED802CEA.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5922-1DD8-B71B-0B83F10A233A0F52.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5923-1DD8-B71B-0B83F66A64E671A0.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5924-1DD8-B71B-0B83F789E92A53A8.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5925-1DD8-B71B-0B83F8D6D2FA8E5F.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5926-1DD8-B71B-0B83F91F54E10B8D.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5927-1DD8-B71B-0B83F9FA6BEBE06B.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5928-1DD8-B71B-0B83FB9DBA83355C.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5929-1DD8-B71B-0B83FCC04874A6A5.jpg" },
-    { url: "https://www.nps.gov/common/uploads/structured_data/3C7D5930-1DD8-B71B-0B83FDD921D8B84C.jpg" }
-  ],
-  contacts: {
-    phoneNumbers: [
-      { phoneNumber: "307-344-7381", type: "Voice" },
-      { phoneNumber: "307-344-2014", type: "Fax" }
-    ]
-  },
-  addresses: [
-    {
-      type: "Mailing",
-      line1: "P.O. Box 168",
-      city: "Yellowstone National Park",
-      stateCode: "WY",
-      postalCode: "82190-0168"
-    },
-    {
-      type: "Physical",
-      line1: "1 Grand Loop Rd",
-      city: "Yellowstone National Park",
-      stateCode: "WY",
-      postalCode: "82190"
-    }
-  ]
-};
+import parkData from "./data/parkData.mjs"; // assumes your dataset file
 
-// new array for the info section
+export function getParkData() {
+  return parkData;
+}
+
 export const parkInfoLinks = [
   {
     name: "Current Conditions &#x203A;",
     link: "conditions.html",
-    image: park.images[2].url,
-    description:
-      "See what conditions to expect in the park before leaving on your trip!"
+    image: parkData.images[2].url,
+    description: "See what conditions to expect in the park before leaving on your trip!"
   },
   {
     name: "Fees and Passes &#x203A;",
     link: "fees.html",
-    image: park.images[3].url,
+    image: parkData.images[3].url,
     description: "Learn about the fees and passes that are available."
   },
   {
     name: "Visitor Centers &#x203A;",
     link: "visitor_centers.html",
-    image: park.images[9].url,
+    image: parkData.images[9].url,
     description: "Learn about the visitor centers in the park."
   }
 ];
-
-export function getParkData() {
-  return park;
-}
